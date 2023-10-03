7103fdc9854f225eb16960a3ac50c6f3d3d66c96
Haidar <afiq.haidar12@gmail.com>
2023-10-03 15:07:21 +0700

    Direct data dummy from seeder

--- database/seeders/GuestSeeder.php
+++ database/seeders/GuestSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use App\Models\Guest;
 
 class GuestSeeder extends Seeder
@@ -13,6 +14,24 @@ class GuestSeeder extends Seeder
      */
     public function run(): void
     {
+        $guests = [
+            [
+                'name' => 'John Doe',
+                'concert' => 'Rock Concert',
+                'date' => now()->addDays(rand(1, 30)),
+                'image' => 'guests/image1.jpg',
+            ],
+            [
+                'name' => 'Jane Smith',
+                'concert' => 'Jazz Night',
+                'date' => now()->addDays(rand(1, 30)),
+                'image' => 'guests/image2.jpg',
+            ],
+
+        ];
+
+        DB::table('guests')->insert($guests);
+
         Guest::factory()->count(5)->create();
     }
 }
